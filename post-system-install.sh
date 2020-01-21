911f8792ba9dce08d7304e77c1582de5a6356db4
Alexander Danilenko <alexander.danilenko@ffwagency.com>
2020-01-21 14:57:43 +0200

    Move offline deb installation to individual script.

--- post-system-install.sh
+++ post-system-install.sh
@@ -2,11 +2,4 @@
 
 set -e
 
-if [ "$(ls -A deb | grep -v "README.md")" ]; then
-  echo "./deb directory has deb files. Installing them first..."
-  sudo dpkg -i deb/*.deb && sudo apt-get install -f
-else
-  echo "./deb directory has no deb files. Skipping..."
-fi
-
 ansible-playbook ansible/_post-system-install.yml --ask-become-pass
