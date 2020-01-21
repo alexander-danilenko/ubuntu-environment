52f992e0dfed80b4533008d914a6a576c9e2817d
Alexander Danilenko <alexander.danilenko@ffwagency.com>
2020-01-21 17:09:19 +0200

    Download dependencies on each install script run.

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ set -e
 
 # Install ansible if not installed yet.
 which ansible 2>/dev/null || { sudo apt-get install -y -q ansible; }
+ansible-galaxy install -r ansible/roles/requirements.yml --roles-path ./ansible/roles
 
 ansible-playbook ansible/_post-system-install.yml --ask-become-pass
