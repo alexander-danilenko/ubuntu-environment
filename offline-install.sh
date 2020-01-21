edd0cc77a2e50c50ee127bfec2ee3e889b52cec3
Alexander Danilenko <alexander.danilenko@ffwagency.com>
2020-01-21 15:39:46 +0200

    Update offline install script for using offline cache directory.

--- offline-install.sh
+++ offline-install.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if [ "$(ls -A deb | grep -v "README.md")" ]; then
-  echo "./deb directory has deb files. Installing them..."
+if [ "$(ls -A ./offline-cache/deb | grep ".deb$")" ]; then
+  echo "./offline-cache/deb directory has deb files. Installing them..."
   sudo dpkg -i offline-cache/deb/*.deb && sudo apt-get install --fix-broken
 else
-  echo "./deb directory has no deb files."
+  echo "./offline-cache/deb directory has no deb files."
   exit 1
 fi
