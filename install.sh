ed42edfeb69503884bcd3c49aae3341cd75d5c24
Alexander Danilenko <alexander.danilenko@ffwagency.com>
2020-01-26 20:52:29 +0200

    Make sure git is installed before running install and offline download scripts.

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-# Install ansible if not installed yet.
+# Install ansible and git if not installed yet.
 which ansible 2>/dev/null || { sudo apt-get install -y -q ansible; }
+which git 2>/dev/null || { sudo apt-get install -y -q git; }
+
 ansible-galaxy install -r ansible/roles/requirements.yml --roles-path ./ansible/roles
 
 ansible-playbook ansible/_post-system-install.yml --ask-become-pass
--- offline-download.sh
+++ offline-download.sh
@@ -4,6 +4,7 @@ set -e
 
 # Install ansible if not installed yet.
 which ansible 2>/dev/null || { sudo apt-get install -y -q ansible; }
+which git 2>/dev/null || { sudo apt-get install -y -q git; }
 
 ansible-galaxy install -r ansible/roles/requirements.yml --roles-path ./ansible/roles
 ansible-playbook ansible/_pre-system-install.yml --ask-become-pass
