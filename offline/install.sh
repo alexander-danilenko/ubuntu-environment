798fd6cc0e83f49b1baaf50ccb6cfc27d05291da
Alexander Danilenko <alexander.danilenko@ffwagency.com>
2020-01-26 22:44:55 +0200

    Update offline install script.

--- offline/install.sh
+++ offline/install.sh
@@ -2,10 +2,21 @@
 
 set -e
 
+################## Helper functions ############################################
+echo_red() {
+    echo  "$(tput setaf 1)$1$(tput sgr0)"
+}
+
+echo_green() {
+    echo  "$(tput setaf 2)$1$(tput sgr0)"
+}
+
+################## Process #####################################################
+
 if [ "$(ls -A ./deb | grep ".deb$")" ]; then
-  echo "./deb directory has deb files. Installing them..."
+  echo_green "./deb directory has deb files. Installing them..."
   sudo dpkg -i offline-cache/deb/*.deb && sudo apt-get install --fix-broken
 else
-  echo "./deb directory has no deb files."
+  echo_red "ERROR: './deb' directory has no deb files."
   exit 1
 fi
