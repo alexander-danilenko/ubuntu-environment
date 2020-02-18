b88e1277581f2a818266f93fea6ee2f03bcb6ca9
Alexander Danilenko <alexander.danilenko@ffwagency.com>
2020-02-18 13:25:48 +0200

    Update formatting and code style.

--- offline/install-deb.sh
+++ offline/install-deb.sh
@@ -3,6 +3,7 @@
 set -e
 
 ################## Helper functions ############################################
+
 echo_red() {
     echo  "$(tput setaf 1)$1$(tput sgr0)"
 }
@@ -11,12 +12,18 @@ echo_green() {
     echo  "$(tput setaf 2)$1$(tput sgr0)"
 }
 
+echo_yellow() {
+    echo  "$(tput setaf 3)$1$(tput sgr0)"
+}
+
 ################## Process #####################################################
 
 if [ "$(ls -A ./deb | grep ".deb$")" ]; then
-  echo_green "./deb directory has deb files. Installing them..."
+  echo_yellow "./deb directory has deb files. Installing them..."
   sudo dpkg -i ./deb/*.deb && sudo apt-get install --fix-broken
 else
   echo_red "ERROR: './deb' directory has no deb files."
   exit 1
 fi
+
+echo_green "Deb files installation was completed."
