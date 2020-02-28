4f355721212524c7ae933bf86202a62a73e1aa62
Alexander Danilenko <alexander.danilenko@ffwagency.com>
2020-02-28 02:33:19 +0200

    Offline scripts: add simple logger.

--- offline/scripts/Driver.py
+++ offline/scripts/Driver.py
@@ -1,4 +1,4 @@
-from .Rainbow import *
+from .Logger import *
 import os
 import requests
 import sys
@@ -29,7 +29,11 @@ class Driver:
     # Response object.
     response: requests.Response
 
+    # Logger.
+    logger: Logger
+
     def __init__(self, url, filename, install_commands, download_dir='./drivers'):
+        self.logger = Logger(f'driver|{filename}')
         self.download_url = url
         self.filename = filename
         self.download_dir = os.path.abspath(download_dir)
@@ -39,9 +43,9 @@ class Driver:
     # Downloads driver to directory with certain filename.
     def download(self):
         if os.path.exists(self.download_file_path):
-            print_blue(f"[driver][{self.filename}] File exists, skipping...")
+            self.logger.info('File exists, skipping...')
             return
-        print_green(f"[driver][{self.filename}] Downloading...")
+        self.logger.log('Downloading...')
 
         # Make sure drivers directory created.
         if not os.path.exists(self.download_dir):
@@ -51,13 +55,13 @@ class Driver:
         try:
             self.response = requests.get(self.download_url)
         except requests.exceptions.RequestException as e:  # This is the correct syntax
-            print_fail(f"[driver][{self.filename}] ERROR: Something went wrong. See details below.")
-            print_fail(e)
+            self.logger.error('ERROR: Something went wrong. See details below.')
+            self.logger.error(e)
             sys.exit(1)
 
         if self.response.status_code != 200:
-            print_fail(f'[driver][{self.filename}] ERROR: Retrieved response with non 200 code for url: {self.download_url}')
-            print_warning('Make sure that URL is correct.')
+            self.logger.error(f'ERROR: Retrieved response with non 200 code for url: {self.download_url}')
+            self.logger.warning('Make sure that URL is correct.')
             sys.exit(1)
 
         # Write downloaded binary file to archive file.
@@ -69,24 +73,24 @@ class Driver:
     # directory.
     def install(self):
         if not os.path.exists(self.download_file_path):
-            print_fail(f'[driver][{self.filename}] ERROR: Driver was not found at {self.download_file_path}')
+            self.logger.error(f'ERROR: Driver was not found at {self.download_file_path}')
             sys.exit(1)
 
         # Create tmp directory before unzipping.
         tmp_dir = tempfile.mkdtemp(f'-{self.filename}-driver')
 
         # Unzip file.
-        print_green(f'[driver][{self.filename}] Unpacking driver to: {tmp_dir}')
+        self.logger.log(f'Unpacking driver to: {tmp_dir}')
         with zipfile.ZipFile(self.download_file_path, 'r') as zip_ref:
             zip_ref.extractall(tmp_dir)
 
         # Convert commands to a string concatenated with ' && '.
         command_line = f'cd {tmp_dir} && {" && ".join(self.install_commands)}'
-        print_green(f'[{self.filename}] Executing install command: "{command_line}"')
+        self.logger.log(f'Executing install command: "{command_line}"')
         subprocess.run(command_line, shell=True)
 
         # Remove driver tmp directory.
-        print_green(f'[driver][{self.filename}] Removing {tmp_dir}')
+        self.logger.log(f'Removing {tmp_dir}')
         shutil.rmtree(tmp_dir)
 
         return self
